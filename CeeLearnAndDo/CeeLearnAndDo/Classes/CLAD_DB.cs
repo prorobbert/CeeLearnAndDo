eb45cda0f2d0c2b4390cba616abd31a47f1f0691
ruben <003random@protonmail.com>
2017-01-14 16:26:30 +0100

    Template erin

--- CeeLearnAndDo/CeeLearnAndDo/Classes/CLAD_DB.cs
+++ CeeLearnAndDo/CeeLearnAndDo/Classes/CLAD_DB.cs
@@ -7,30 +7,30 @@ namespace CeeLearnAndDo.Classes
 {
     public class CLAD_DB
     {
-        string connect;
+        //string connect;
 
-        public List<ProjectProp> GetProjects()
-        {
-            List<ProjectProp> projects;
-            return projects;
-        }
+        //public List<ProjectProp> GetProjects()
+        //{
+        //    List<ProjectProp> projects;
+        //    return projects;
+        //}
 
-        public List<ProjectProp> GetFaQ()
-        {
-            List<ProjectProp> faqs;
-            return faqs;
-        }
+        //public List<ProjectProp> GetFaQ()
+        //{
+        //    List<ProjectProp> faqs;
+        //    return faqs;
+        //}
 
-        public List<ProjectProp> GetMailInfo()
-        {
-            List<ProjectProp> mailInfo;
-            return mailInfo;
-        }
+        //public List<ProjectProp> GetMailInfo()
+        //{
+        //    List<ProjectProp> mailInfo;
+        //    return mailInfo;
+        //}
 
-        public List<ProjectProp> GetUser()
-        {
-            List<ProjectProp> users;
-            return users;
-        }
+        //public List<ProjectProp> GetUser()
+        //{
+        //    List<ProjectProp> users;
+        //    return users;
+        //}
     }
 }
