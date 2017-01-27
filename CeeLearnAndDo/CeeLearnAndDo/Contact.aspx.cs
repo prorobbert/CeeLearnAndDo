5ec298ce8645daf7ba83ccae00bf1a817bcf1a57
Robbert Ruiter <ro-ru@live.nl>
2017-01-27 11:04:27 +0100

    fagsfgsfdg

--- CeeLearnAndDo/CeeLearnAndDo/Contact.aspx.cs
+++ CeeLearnAndDo/CeeLearnAndDo/Contact.aspx.cs
@@ -18,6 +18,30 @@ namespace CeeLearnAndDo
         protected void btnVerzenden_Click(object sender, EventArgs e)
         {
             //Code om een mail te versturen met de vraag
+
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.To.Add("ruben_bos40@protonmail.com");
+            mail.From = new MailAddress("smoothboardsmailer@gmail.com", tbEmail.Text, System.Text.Encoding.UTF8);
+            mail.Subject = "CeeLearnAndDo vraag";
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = " Time: " + DateTime.Now.ToString("h:mm:ss tt") + "Van: " + tbName.Text + "Vraag: " + tbVraag.Text;
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("smoothboardsmailer@gmail.com", "MyPassword123");
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            try
+            {
+                client.Send(mail);
+            }
+            catch
+            {
+
+                Response.Write("RIP, everything fails today, i wanted to send myself a mail with what just happend but even that part failed :'(");
+            }
         }
     }
 }
